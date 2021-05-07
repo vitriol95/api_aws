892cef04afb0e4c44cd9dfee78f7eeedfa2d12e3
장세현 <vitriol951@gmail.com>
2021-05-07 15:35:26 +0900

    Initial commit

---
+++ src/main/java/vitriol/api_aws/ApiAwsApplication.java
@@ -0,0 +1,13 @@
+package vitriol.api_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vitriol/api_aws/ApiAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package vitriol.api_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
